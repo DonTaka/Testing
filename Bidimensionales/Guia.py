f35b4ced2bbbb7dc10a0326fed77a4f657bc8aa2
DonTaka <jo.riquelmees@gmail.com>
2022-06-08 12:42:01 -0400

    Desarrollo Guia

---
+++ Bidimensionales/Guia.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+
+#Ejercicio 1
+m1 = np.random.randint(0,100,size=(3,3))
+m2 = np.random.randint(0,100,size=(3,3))
+print(m1)
+print(m2)
+m3 = m1*m2;
+print(m3)
+
+#Ejercicio 2
+bus = np.zeros([10,4])
+print(bus)
